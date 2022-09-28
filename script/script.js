c33150e74e8c928270e0dcba981f56c2984823e5
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-28 19:28:43 +0200

    pulsante avanti funziona

--- script/script.js
+++ script/script.js
@@ -1,20 +1,15 @@
-// function avantiImmagine() {
-//   let im1 = document.getElementById("im1");
-
-//   let im2 = document.getElementById("im2");
-
-//   let im3 = document.getElementById("im3");
-
-//   let im4 = document.getElementById("im4");
-
-//   let im5 = document.getElementById("im5");
-// }
 let numeroDiPagina = 0;
-function mostraImmagine() {
-  numeroDiPagina = numeroDiPagina + 1;
-  let immagineCorrente = document.getElementById("im" + numeroDiPagina);
-  immagineCorrente.style.display = "flex";
-  if (immagineCorrente > numeroDiPagina) {
-    immagineCorrente.style.display = "none";
+const tutteLePagine = document.getElementsByClassName("slider-item");
+
+const avanti = function () {
+  for (let i = 0; i < tutteLePagine.length; i++) {
+    tutteLePagine[i].classList.remove("mostra");
   }
-}
+  numeroDiPagina++;
+  console.log(numeroDiPagina);
+  if (numeroDiPagina >= tutteLePagine.length) {
+    numeroDiPagina = 0;
+  }
+  const paginaCorrente = tutteLePagine[numeroDiPagina];
+  paginaCorrente.classList.add("mostra");
+};
